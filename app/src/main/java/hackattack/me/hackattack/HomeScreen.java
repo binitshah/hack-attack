11047f0571f082f14a87ee7e8e089e92e205f547
Andrew <akandroid21@gmail.com>
2017-01-21 10:46:36 -0500

    asf

--- app/src/main/java/hackattack/me/hackattack/HomeScreen.java
+++ app/src/main/java/hackattack/me/hackattack/HomeScreen.java
@@ -1,25 +1,95 @@
 package hackattack.me.hackattack;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 public class HomeScreen extends AppCompatActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    public Context context;
+    public Button joinbutton;
+    public EditText serverCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+        context = this;
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+
         Button hostbutton = (Button) findViewById(R.id.hostbutton);
+        joinbutton = (Button) findViewById(R.id.joinbutton);
+        serverCode = (EditText) findViewById(R.id.serverCode);
+        serverCode.setVisibility(View.INVISIBLE);
 
         hostbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //where the server code goes
+                new AlertDialog.Builder(context)
+                    .setTitle("Host")
+                    .setMessage("Server code: ASDF")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // continue with delete
+                        }
+                    })
+                    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // do nothing
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
             }
         });
+
+        joinbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serverCode.setVisibility(View.VISIBLE);
+                joinbutton.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
     }
 }
